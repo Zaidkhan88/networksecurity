0bfaeb8e99e025ec2e8e63b2b4b9416bcd14f86b
Zaidkhan88 <zaidk8517@gmail.com>
2026-05-10 11:29:58 +0530

    ETL pipeline completed

---
+++ push_data.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+print(MONGO_DB_URL)
+
+import certifi
+ca = certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)    
+        
+    def csv_to_json(self, file_path):
+        try:
+            df = pd.read_csv(file_path)
+            df.reset_index(inplace=True, drop=True)
+            records = list(json.loads(df.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def insert_data_to_mongodb(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+            
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+
+if __name__ == "__main__":
+    try:
+        network_data_extract = NetworkDataExtract()
+        records = network_data_extract.csv_to_json(file_path="Network_Data/phisingData.csv")
+        print(records)
+        print(network_data_extract.insert_data_to_mongodb(records=records, database="network_security", collection="training_set"))
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from pymongo import MongoClient
+# from pymongo.server_api import ServerApi
+# uri = "mongodb+srv://zaidk8516_db_user:admin1234@cluster0.ohcx1lw.mongodb.net/?appName=Cluster0"
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
+
+
