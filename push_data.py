6e7411ca2ae59b5bf70d7053de61d85e8f2692b5
Zaidkhan88 <zaidk8517@gmail.com>
2026-05-10 11:30:57 +0530

    Improvement in push_data.py

--- push_data.py
+++ push_data.py
@@ -73,18 +73,3 @@ if __name__ == "__main__":
 
 
 
-
-# from pymongo import MongoClient
-# from pymongo.server_api import ServerApi
-# uri = "mongodb+srv://zaidk8516_db_user:admin1234@cluster0.ohcx1lw.mongodb.net/?appName=Cluster0"
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
-
-
